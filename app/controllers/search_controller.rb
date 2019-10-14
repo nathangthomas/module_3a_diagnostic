20f1d391531a9834f01ad8e224af31ca6d0a0b25
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-14 11:23:12 -0600

    Refactor service and delete commented out code in the controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,24 +14,3 @@ class SearchController < ApplicationController
     end
   end
 end
-# def parsed_data
-#   response = Faraday.get("https://www.potterapi.com/v1/characters?key=$2a$10$Xrj4w4pg4fk5RB85JrNGhO8RdMJEEGTbF/RGyoaV1gqE9s2Wvgo2K&house=Grffindor&bloodStatus=half-blood")
-#   JSON.parse(response.body, symbolize_names: true)
-# end
-
-  # def conn
-  #   Faraday.new(
-  #   url: "https://www.potterapi.com/v1",
-  #   params: {key: "$2a$10$Xrj4w4pg4fk5RB85JrNGhO8RdMJEEGTbF/RGyoaV1gqE9s2Wvgo2K", house: "Slytherin", bloodStatus: "half-blood"},
-  #   headers: {'Content-Type' => 'application/json'}
-  #   )
-  # end
-  #
-  # def response
-  #   resp = conn.get('characters') do |req|
-  #     req.params
-  #   end
-  #   JSON.parse(resp.body, symbolize_names: true)
-  # end
-
-# end
--- app/services/house_service.rb
+++ app/services/house_service.rb
@@ -1,5 +1,20 @@
 class HouseService
+#   def get_data
+#     conn = Faraday.get("https://www.potterapi.com/v1/characters?key=#{ENV['KEY']}&house=Gryffindor")
+#   end
+# end
+
+  def conn
+    Faraday.new(
+    url: "https://www.potterapi.com/v1",
+    params: {key: ENV['KEY'], house: "Gryffindor"},
+    headers: {'Content-Type' => 'application/json'}
+  )
+  end
+
   def get_data
-    conn = Faraday.get("https://www.potterapi.com/v1/characters?key=#{ENV['KEY']}&house=Gryffindor")
+    conn.get('characters') do |req|
+      req.params
+    end
   end
 end
